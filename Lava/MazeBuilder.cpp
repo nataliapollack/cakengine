97f977622bf8a97155ad812975a2a6a4d286b0b6
Braedan Nevers <71280413+Braedan-Nevers@users.noreply.github.com>
2026-02-01 14:49:57 -0800

    Maze building correctly

--- Lava/MazeBuilder.cpp
+++ Lava/MazeBuilder.cpp
@@ -10,43 +10,61 @@ extern Coordinator gCoordinator;
 void MazeBuilder::BuildMaze()
 {
 	// Get random starting cell in maze
-	std::random_device rd;
-	std::mt19937 eng(rd());
-	std::uniform_int_distribution<> distr(0, MazeSize - 1);
+	if (firstStep)
+	{
+		std::random_device rd;
+		std::mt19937 eng(rd());
+		std::uniform_int_distribution<> distr(0, MazeWidth-1);//MazeSize - 1);
+		std::uniform_int_distribution<> distrRow(0, MazeHeight - 1);
+		int randomCol = distr(eng);
+		int randomRow = distrRow(eng);
 
-	int randomRow = distr(eng);
-	int randomCol = distr(eng);
-
-	MazeNode* InitialNode = &MazeNodes[randomRow][randomCol];
+		MazeNode* InitialNode = MazeNodes[randomCol][randomRow];
 
 
-	InitialNode->Visited = true;
-	VisitedNodes.push(InitialNode);
+		InitialNode->Visited = true;
+		VisitedNodes.push(InitialNode);
+		firstStep = false;
+	}
+
 
 	while (!VisitedNodes.empty())
 	{
 		MazeNode* currNode = VisitedNodes.top();
 		VisitedNodes.pop();
 
+		currNode->OnStack = false;
+
 		std::vector<MazeNode*> neighbors = GetValidNeighbors(currNode);
 		if (!neighbors.empty())
 		{
 			VisitedNodes.push(currNode);
+			currNode->OnStack = true;
 
 			std::random_device rd;
 			std::mt19937 eng(rd());
 			std::uniform_int_distribution<> distr(0, neighbors.size() - 1);
 			int randomIndex = distr(eng);
 
-			//TODO: Place wall or whatever, on chosen node's location
+			// Deletes walls between currNode and chosen node
+			MazeNode* chosenNode = neighbors[randomIndex];
+			currNode->walls[chosenNode->ParentToThis] = false;
+			chosenNode->walls[chosenNode->ThisToParent] = false;
 
 
 			neighbors[randomIndex]->Visited = true;	
 			VisitedNodes.push(neighbors[randomIndex]);
+			neighbors[randomIndex]->OnStack = true;
 
 		}
 		// else means we hit a dead end so maybe do something here if we want
+
+		if (StepByStepBuild)
+		{
+			return;
+		}
 	}
+	mazeBuilt = true;
 
 }
 
@@ -56,63 +74,127 @@ std::vector<MazeBuilder::MazeNode*> MazeBuilder::GetValidNeighbors(MazeNode* nod
 	int NeighborCol;
 	int NeighborRow;
 
-	// Check North
+	// Check South
 	NeighborRow = node->row + 1;
 	NeighborCol = node->col;
-	if (NeighborRow < MazeSize)
+	if (NeighborRow < MazeHeight)
 	{
-		MazeNode* North = &MazeNodes[NeighborRow][NeighborCol];
-		if (North->Visited == false)
-			ValidNeighbors.push_back(North);
+		MazeNode* South = MazeNodes[NeighborCol][NeighborRow];
+		if (South->Visited == false)
+		{
+			ValidNeighbors.push_back(South);
+			South->ParentToThis = SOUTH;
+			South->ThisToParent = NORTH;
+		}
 	}
 	// Check East
 	NeighborRow = node->row;
 	NeighborCol = node->col + 1;
-	if (NeighborCol < MazeSize)
+	if (NeighborCol < MazeWidth)
 	{
-		MazeNode* East = &MazeNodes[NeighborRow][NeighborCol];
+		MazeNode* East = MazeNodes[NeighborCol][NeighborRow];
 		if (East->Visited == false)
+		{
 			ValidNeighbors.push_back(East);
+			East->ParentToThis = EAST;
+			East->ThisToParent = WEST;
+		}
 	}
-	// Check South
+	// Check North
 	NeighborRow = node->row - 1;
 	NeighborCol = node->col;
 	if (NeighborRow >= 0)
 	{
-		MazeNode* South = &MazeNodes[NeighborRow][NeighborCol];
-		if (South->Visited == false)
-			ValidNeighbors.push_back(South);
+		MazeNode* North = MazeNodes[NeighborCol][NeighborRow];
+		if (North->Visited == false)
+		{
+			ValidNeighbors.push_back(North);
+			North->ParentToThis = NORTH;
+			North->ThisToParent = SOUTH;
+		}
 	}
 	// Check West
 	NeighborRow = node->row;
 	NeighborCol = node->col - 1;
 	if (NeighborCol >= 0)
 	{
-		MazeNode* West = &MazeNodes[NeighborRow][NeighborCol];
+		MazeNode* West = MazeNodes[NeighborCol][NeighborRow];
 		if (West->Visited == false)
+		{
 			ValidNeighbors.push_back(West);
+			West->ParentToThis = WEST;
+			West->ThisToParent = EAST;
+		}
 	}
 	return ValidNeighbors;
+	//return std::vector<MazeNode*>();
+}
+
+void MazeBuilder::StartMaze()
+{
+	for (int i = 0; i < MazeWidth; ++i)
+		for (int j = 0; j < MazeHeight; ++j)
+		{
+			// TODO: Remember to Delete this shit somewhere! 
+			MazeNodes[i][j] = new MazeNode;
+			MazeNodes[i][j]->col = i;
+			MazeNodes[i][j]->row = j;
+		}
+	if(!StepByStepBuild)
+		BuildMaze();
+}
+
+void MazeBuilder::UpdateMaze(float dt)
+{
+
+}
+
+bool MazeBuilder::GetIsActive()
+{
+	return isActive;
 }
 
 void MazeBuilder::PlaceWall(Vector2 pos)
 {
-	Entity en = gCoordinator.CreateEntity();
-	gCoordinator.AddComponent(
-		en,
-		render_box{ Vector2{10.0f, 10.0f }, BLACK }
-	);
 
-	gCoordinator.AddComponent(
-		en,
-		transform2D{ pos }
-	);
+}
 
-	gCoordinator.AddComponent(
-		en,
-		status{ true, true, MAZE, WALL }
+void MazeBuilder::DrawMaze()
+{
+	//DrawRectangle(0.0f, 0.0f, node.nodeSize, node.nodeSize, WHITE);
 
-	);
+	//DrawRectangle(node.nodeSize * 11, 0.0f, node.nodeSize, node.nodeSize, WHITE);
+	//DrawRectangle(0.0f, node.nodeSize * 7, node.nodeSize, node.nodeSize, WHITE);
 
 
+	for (int col = 0; col < MazeWidth; ++col)
+	{
+		for (int row = 0; row < MazeHeight; ++row)
+		{
+			MazeNode* node = MazeNodes[col][row];
+			if (!node->Visited)
+				continue;
+			// Draw nodes
+			int xPos = col * nodeSize;
+			int yPos = row * nodeSize;
+			if(node->Visited)
+				DrawRectangle(xPos, yPos, nodeSize, nodeSize, WHITE);
+			if(node->OnStack)
+				DrawRectangle(xPos, yPos, nodeSize, nodeSize, GREEN);
+
+			// Draw walls 
+			// North
+			if(node->walls[NORTH])
+				DrawRectangle(xPos, yPos, nodeSize, wallThickness, PURPLE);
+			// East
+			if (node->walls[EAST])
+				DrawRectangle(xPos + wallOffset, yPos, wallThickness, nodeSize, PURPLE);
+			// South
+			if (node->walls[SOUTH])
+				DrawRectangle(xPos, yPos + wallOffset, nodeSize, wallThickness, PURPLE);
+			// West
+			if (node->walls[WEST])
+				DrawRectangle(xPos, yPos, wallThickness, nodeSize, PURPLE);
+		}
+	}
 }
--- Lava/MazeBuilder.h
+++ Lava/MazeBuilder.h
@@ -2,33 +2,90 @@
 #include <stack>
 #include <vector>
 #include "raylib.h"
+#include "Events.hpp"
+
+
 // Ok uhh, this will build the maze and place whatever items in it, 
 // but won't really be doing much actively 
-
-
 
 class MazeBuilder
 {
 public:
+	// Just for some extra readability 
+	enum WallDirs
+	{
+		NORTH = 0,
+		EAST = 1,
+		SOUTH = 2,
+		WEST = 3,
+		NONE = 5
+	};
+
+	void StartMaze();
+	void UpdateMaze(float dt);
+	//void NewDay(Event& event);
+	bool GetIsActive();
+
+
+
+	void PlaceWall(Vector2 pos);
+	void DrawMaze();
+	bool mazeBuilt = false;
+	void BuildMaze();
+	
 	struct MazeNode
 	{
 		bool Visited = false;
+		bool OnStack = false; // Just a debugging thing
 		int row;
 		int col;
+
+		// If parent is 0,0 and this is 1,0
+		// This variable would be EAST
+		// 
+		WallDirs ParentToThis = NONE;
+		WallDirs ThisToParent = NONE; 
+
+		// True if the wall still exists
+		bool walls[4] = { true, true, true, true };
 	};
 
 private:
-	static const int MazeSize = 40;
 
-	MazeNode MazeNodes[MazeSize][MazeSize];
+	// Ok just some stuff to remind me when I inevitably need to change this
+	// Width == num of cols == X-axis! Height == num of Rows == Y-axis! 
+	// (Stop confusing them aaaa!)
+	// 
+	// To get these vals I was just trial and error-ing it,
+	// place a cell at 0.0, another at x * nodeSize, replace x till we find the corners
+	// (I'm sure there's some funny math way to do it with screen size 
+	// and desired number of nodes and node size yadda yadda, 
+	// but i don't wanna think that much rn
+	// 
+	// Good numbies with nodeSize 50 are:
+	// width = 18, height = 12, thickness = 10 
+	static const int MazeWidth = 18;
+	static const int MazeHeight = 12;
+
+	
+
+	MazeNode* MazeNodes[MazeWidth][MazeHeight];
+
+	int nodeSize = 50;
+	float wallOffset = nodeSize;
+
+	// Just whatever looks good really
+	int wallThickness = 10;
 
 	std::stack<MazeNode*> VisitedNodes;
 
-	void BuildMaze();
+
+	bool isActive = false;
+	bool StepByStepBuild = true;
+	bool firstStep = true;
 
 	std::vector<MazeNode*> GetValidNeighbors(MazeNode* node);
 
-	void PlaceWall(Vector2 pos);
 
 };
 
