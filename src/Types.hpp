5ecbcafc709cdc790d7d5000d027b52701689837
doing-it-sideways <nohitruns73@gmail.com>
2026-02-02 00:27:39 -0500

    more inlining and unreachable definition (used in AssetManager)

--- src/Types.hpp
+++ src/Types.hpp
@@ -5,11 +5,11 @@
 
 // ECS sets
 using Entity = std::uint32_t;
-const Entity MAX_ENTITIES = 5000;
+const inline Entity MAX_ENTITIES = 5000;
 using ComponentType = std::uint8_t;
-const ComponentType MAX_COMPONENTS = 32;
+const inline ComponentType MAX_COMPONENTS = 32;
 using Signature = std::bitset<MAX_COMPONENTS>;
-const int MAX_SYSTEMS = 128;
+const inline int MAX_SYSTEMS = 128;
 
 // Source: https://gist.github.com/Lee-R/3839813
 constexpr std::uint32_t fnv1a_32(char const* s, std::size_t count)
@@ -28,3 +28,13 @@ using ParamId = std::uint32_t;
 
 #define METHOD_LISTENER(EventType, Listener) EventType, std::bind(&Listener, this, std::placeholders::_1)
 #define FUNCTION_LISTENER(EventType, Listener) EventType, std::bind(&Listener, std::placeholders::_1)
+
+#if defined(__cpp_lib_unreachable)
+#define CAKE_UNREACHABLE std::unreachable()
+#elif defined(__GNUC__) || defined(__clang__)
+#define CAKE_UNREACHABLE __builtin_unreachable()
+#elif defined(_MSC_VER)
+#define CAKE_UNREACHABLE __assume(0)
+#else
+#define CAKE_UNREACHABLE
+#endif // _MSC_VER
